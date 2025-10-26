67bc767154ddbd3523f5d409947d23f2eedb3dfa
abhishek-kumar <abhishekkumar@abhisheks-Mac-mini.local>
2025-10-26 12:21:12 +0530

    cont loops

--- 02_Conditionals_Loops/problemsolving_loops.py
+++ 02_Conditionals_Loops/problemsolving_loops.py
@@ -399,5 +399,314 @@ def data_types (lis1):
     for i in lis1:
         print(i,type(i))
     
-data_types(lis1)
+# data_types(lis1)
 
+'''
+8. Print Numbers 0 to 6 Except 3 and 6
+
+Write a Python program that prints all the numbers from 0 to 6 except 3 and 6.
+
+Note : Use 'continue' statement.
+
+Expected Output : 0 1 2 4 5
+
+Click me to see the sample solution
+
+'''
+
+# num = 6
+
+# for i in range(0,7):
+    
+'''
+9. Fibonacci Series Between 0 and 50
+
+Write a Python program to get the Fibonacci series between 0 and 50.
+
+Note : The Fibonacci Sequence is the series of numbers :
+
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, ....
+Every next number is found by adding up the two numbers before it.
+
+Expected Output : 1 1 2 3 5 8 13 21 34
+
+Click me to see the sample solution
+'''
+pass
+
+'''
+10. FizzBuzz Variation
+
+Write a Python program that iterates the integers from 1 to 50. For multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz". For numbers that are multiples of three and five, print "FizzBuzz".
+
+Sample Output :
+
+fizzbuzz
+1
+2
+fizz
+4
+buzz
+
+Click me to see the sample solution
+
+'''
+pass
+
+'''
+11. Two-Dimensional Array (Multiplication Table)
+
+Write a Python program that takes two digits m (row) and n (column) as input and generates a two-dimensional array. 
+The element value in the i-th row and j-th column of the array should be i*j.
+
+Note :
+i = 0,1.., m-1
+j = 0,1, n-1.
+
+Test Data : Rows = 3, Columns = 4
+Expected Result : [[0, 0, 0, 0], [0, 1, 2, 3], [0, 2, 4, 6]]
+
+Click me to see the sample solution
+
+'''
+pass
+
+'''
+
+Python Conditional Statements and loops
+Last update on March 26 2025 08:12:01 (UTC/GMT +8 hours)
+
+Discover more
+Python Programming
+python
+Java programming resources
+This resource offers a total of 220 Python conditional statements and loops problems for practice. It includes 44 main exercises, each accompanied by solutions, detailed explanations, and four related problems.
+
+[An Editor is available at the bottom of the page to write and execute the scripts.]
+
+
+1. Divisible by 7 and Multiples of 5
+
+Write a Python program to find those numbers which are divisible by 7 and multiples of 5, between 1500 and 2700 (both included).
+
+Click me to see the sample solution
+
+
+2. Temperature Converter
+
+Write a Python program to convert temperatures to and from Celsius and Fahrenheit.
+
+[ Formula : c/5 = f-32/9 [ where c = temperature in celsius and f = temperature in fahrenheit ]
+
+Expected Output :
+
+60°C is 140 in Fahrenheit
+45°F is 7 in Celsius
+
+Click me to see the sample solution
+
+
+3. Number Guessing Game
+
+Write a Python program to guess a number between 1 and 9.
+
+Note : User is prompted to enter a guess. If the user guesses wrong then the prompt appears again until the guess is correct, on successful guess, user will get a "Well guessed!" message, and the program will exit.
+
+Click me to see the sample solution
+
+
+4. Construct Pattern (Diamond Pattern)
+
+Write a Python program to construct the following pattern, using a nested for loop.
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
+Click me to see the sample solution
+
+
+5. Reverse a Word
+
+Write a Python program that accepts a word from the user and reverses it.
+
+Click me to see the sample solution
+
+
+6. Count Even and Odd Numbers
+
+Write a Python program to count the number of even and odd numbers in a series of numbers
+
+Sample numbers : numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
+
+Expected Output :
+
+Number of even numbers : 5
+Number of odd numbers : 4
+
+Click me to see the sample solution
+
+
+7. Print Items with Types
+
+Write a Python program that prints each item and its corresponding type from the following list.
+
+Sample List : datalist = [1452, 11.23, 1+2j, True, 'w3resource', (0, -1), [5, 12], {"class":'V', "section":'A'}]
+
+Click me to see the sample solution
+
+
+8. Print Numbers 0 to 6 Except 3 and 6
+
+Write a Python program that prints all the numbers from 0 to 6 except 3 and 6.
+
+Note : Use 'continue' statement.
+
+Expected Output : 0 1 2 4 5
+
+Click me to see the sample solution
+
+
+9. Fibonacci Series Between 0 and 50
+
+Write a Python program to get the Fibonacci series between 0 and 50.
+
+
+Note : The Fibonacci Sequence is the series of numbers :
+
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, ....
+Every next number is found by adding up the two numbers before it.
+
+Expected Output : 1 1 2 3 5 8 13 21 34
+
+Click me to see the sample solution
+
+
+10. FizzBuzz Variation
+
+Write a Python program that iterates the integers from 1 to 50. For multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz". For numbers that are multiples of three and five, print "FizzBuzz".
+
+Sample Output :
+
+fizzbuzz
+1
+2
+fizz
+4
+buzz
+
+Click me to see the sample solution
+
+
+11. Two-Dimensional Array (Multiplication Table)
+
+Write a Python program that takes two digits m (row) and n (column) as input and generates a two-dimensional array. The element value in the i-th row and j-th column of the array should be i*j.
+
+Note :
+i = 0,1.., m-1
+j = 0,1, n-1.
+
+Test Data : Rows = 3, Columns = 4
+Expected Result : [[0, 0, 0, 0], [0, 1, 2, 3], [0, 2, 4, 6]]
+
+Click me to see the sample solution
+
+
+'''
+
+pass
+'''
+12. Sequence of Lines to Lowercase
+
+Write a Python program that accepts a sequence of lines (blank line to terminate) as input and prints the 
+
+lines as output (all characters in lower case).
+
+Click me to see the sample solution
+
+
+'''
+pass
+
+'''
+14. Count Digits and Letters in a String
+
+Write a Python program that accepts a string and calculates the number of digits and letters.
+
+Sample Data : Python 3.2
+
+Expected Output :
+
+Letters 6
+Digits 2
+
+Click me to see the sample solution
+
+'''
+
+str1 = 'W3resource'   
+
+def count_letter_digit (str1):
+    letter = []
+    digit = []
+    for i in str1:
+        if i == str(i):
+            d = letter.append(i)
+            len(letter)
+        elif i == int(i):
+            digit.append(i)
+            lt = len(digit)
+
+        print(d)
+    return
+
+
+# count_letter_digit(str1)
+
+'''
+15. Password Validity Checker
+
+Write a Python program to check the validity of passwords input by users.
+
+Validation :
+
+At least 1 letter between [a-z] and 1 letter between [A-Z].
+At least 1 number between [0-9].
+At least 1 character from [$#@].
+Minimum length 6 characters.
+Maximum length 16 characters.
+Click me to see the sample solution
+'''
+
+pass
+
+'''
+16. Numbers with All Even Digits
+
+Write a Python program to find numbers between 100 and 400 (both included) where each digit of a number is an even number. 
+
+
+The numbers obtained should be printed in a comma-separated sequence.
+
+Click me to see the sample solution
+
+
+'''
+
+num1 =100
+num2 = 400
+
+def even_interval(num1,num2):
+    for i in range(num1,num2+1):
+        if i%2 ==0:
+            print(i)
+
+
+even_interval(num1,num2)
